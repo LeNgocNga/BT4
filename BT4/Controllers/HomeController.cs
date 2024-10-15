fb3951d65b3ee7315b09ef90ba77a8b2aaf69793
ASUS PC <22520927@gm.uit.edu.vn>
2024-10-15 16:04:43 +0700

    Add project files.

---
+++ BT4/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using BT4.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BT4.Controllers
+{
+    public class HomeController : Controller
+    {
+        QlbanVaLiContext db = new QlbanVaLiContext();
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var lstsanpham = db.TDanhMucSps.ToList();
+            return View(lstsanpham);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
